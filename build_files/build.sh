4126a72149d538c88143ed0e88ff5ef08ccdd9ef
Daher12 <133640261+Daher12@users.noreply.github.com>
2025-08-30 13:38:22 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ dnf5 -y copr enable scottames/ghostty
 dnf5 -y copr enable chenxiaolong/sbctl
 dnf5 -y config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 
-dnf5 install -y ghostty fish nautilus corectrl sbctl tailscale gnome-shell gnome-tweaks tlp --setopt=install_weak_deps=False
+dnf5 install -y ghostty fish nautilus sbctl tailscale gnome-shell gnome-tweaks tlp --setopt=install_weak_deps=False
 
 rm /etc/yum.repos.d/tailscale.repo /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:chenxiaolong:sbctl.repo /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ublue-os:akmods.repo
 
