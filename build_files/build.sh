4ed21db368bb06c4f42dda9ada1bd9e5a5474da1
Daher12 <133640261+Daher12@users.noreply.github.com>
2025-09-04 11:37:29 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,10 +16,6 @@ rm /etc/yum.repos.d/tailscale.repo /etc/yum.repos.d/_copr:copr.fedorainfracloud.
 
 ## Nix
 mkdir -p /nix 
-mkdir -p /usr/dp
-
-curl -L https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v6.1.1-1/fedora-42-displaylink-1.14.10-1.github_evdi.x86_64.rpm -o /usr/dp/dp.rpm && \
-dnf5 install -y /usr/dp/dp.rpm
 
 ## Ox
 curl -L https://github.com/curlpipe/ox/releases/latest/download/ox -o /usr/bin/ox && \
